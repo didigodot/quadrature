5bb27050caccee3cf6fdfe6414519512a6a435c7
Didi Park <2015dpark2@tjhsst.edu>
2013-05-10 12:56:42 -0400

    experiment

--- panel.java
+++ panel.java
@@ -10,8 +10,8 @@ public class panel extends JPanel
         g.drawLine(50, 550, 700, 550);
         g.drawLine(50, 550, 50, 50);
         for(int i = 50; i<700; i++){
-            int y = (int)(Math.pow(Math.E, (double)((i-50)/350)));
-            g.drawRect(i, 100*(y+300), 1, 1);
+            int y = (int)(Math.pow(Math.E, (double)((i-50)/250)));
+            g.drawRect(i, (int)(0.8*(y+300)), 1, 1);
             }
     }
 }
