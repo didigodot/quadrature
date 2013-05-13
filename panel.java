4c6c95865413e126987c90220e994916d0bb9aab
Dmitri Park <cellofortist@gmail.com>
2013-05-13 17:43:12 -0400

    modular

--- panel.java
+++ panel.java
@@ -5,15 +5,20 @@ import java.util.*;
 
 public class panel extends JPanel 
 {
-    public void paintComponent(Graphics g) 
+   public void drawAxes(Graphics g)
     {
-        
         g.setColor(Color.BLACK);
         g.drawLine(50, 550, 700, 550);
         g.drawLine(50, 550, 50, 50);
-        for(int i = 50; i<700; i++){
+    } 
+
+    public void paintComponent(Graphics g) 
+    {
+        drawAxes(g);
+        for(int i = 50; i<700; i++)
+            {
             double y = Math.pow((((double)i-50.0)/130.0), 2.0);
-	    System.out.println(y);
+	        System.out.println(y);
             g.drawRect(i, (int)((550-(y*22))), 0, 1);
             }
     }
