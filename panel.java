92924822d78c153b6d2599383da9c772de9941f3
Dmitri Park <cellofortist@gmail.com>
2013-05-13 17:29:25 -0400

    finally figured out my failures...

--- panel.java
+++ panel.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.lang.Math;
+import java.util.*;
+
 public class panel extends JPanel 
 {
     public void paintComponent(Graphics g) 
@@ -10,8 +12,9 @@ public class panel extends JPanel
         g.drawLine(50, 550, 700, 550);
         g.drawLine(50, 550, 50, 50);
         for(int i = 50; i<700; i++){
-            int y = (int)(Math.pow(Math.E, (double)((i-50)/250)));
-            g.drawRect(i, (int)(0.8*(y+300)), 1, 1);
+            double y = Math.pow((((double)i-50.0)/130.0), 2.0);
+	    System.out.println(y);
+            g.drawRect(i, (int)((550-(y*22))), 0, 1);
             }
     }
 }
