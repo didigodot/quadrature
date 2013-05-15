72a34a732c604e046c5f5e3f13006b34b5333060
Didi Park <cellofortist@gmail.com>
2013-05-15 09:40:34 -0400

    stuff

--- panel.java
+++ panel.java
@@ -2,24 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.lang.Math;
 import java.util.*;
-
+import java.awt.event.*;
 public class panel extends JPanel 
 {
-   public void drawAxes(Graphics g)
+    private JLabel title; //"Quadrature"
+    private JTextField input; //Box to input function
+    public panel()
+    {
+        setLayout(new FlowLayout());
+        title = new JLabel("Quadrature");
+        title.setFont(new Font("Helvetica", Font.BOLD, 40));
+        title.setForeground(Color.blue);
+        add(title);
+    
+        input = new JTextField("0.0, 7");
+        add(input);
+
+        JButton run = new JButton("run");
+        run.addActionListener(new Listener());
+        add(run);
+    }
+    public void drawAxes(Graphics g)
     {
         g.setColor(Color.BLACK);
         g.drawLine(50, 550, 700, 550);
         g.drawLine(50, 550, 50, 50);
     } 
-
+    
     public void paintComponent(Graphics g) 
     {
         drawAxes(g);
         for(int i = 50; i<700; i++)
             {
             double y = Math.pow((((double)i-50.0)/130.0), 2.0);
-            g.drawRect(i, (int)((550-(y*22))), 0, 1);
+            g.drawRect(i, (int)((550-(y*22))), 0, 0);
             }
     }
+    private class Listener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println(input.getText());
+        }
+    }
 }
-	    
