9e8b860d7cd4a756861cb0e91e41feeef39dc185
Dmitri Park <cellofortist@gmail.com>
2013-05-23 10:11:21 -0400

    processor stubs javadoc style

--- quadrature-stubs/Processor.java
+++ quadrature-stubs/Processor.java
@@ -6,21 +6,44 @@
 
 public class Processor
 {
+    /*************************************************
+    * Computes integral using trapezoidal rule
+    * @param x  data file name
+    * @param a  integration domain
+    * @return   answer
+    ************************************************/
     public static double trapezoidal(String x, int[] a)
     {
         //Uses trapezoidal rule to compute integral
         return -1;
     }
+	 /*************************************************
+    * Computes integral using Simpson's rule
+    * @param x  data file name
+    * @param a  integration domain
+    * @return   answer
+    ************************************************/
     public static double simpsons(String x, int[] a)
     {
         //Uses Simpson's rule to compute integral
         return -1;
     }
+	 /************************************************
+    * Computes integral using Boole's rule
+    * @param x  data file name
+    * @param a  integration domain
+    * @return   answer
+    ************************************************/
     public static double booles(String x, int[] a)
     {
         //Uses Boole's rule to compute intregral
         return -1
     }
+	 /************************************************
+    * Redirects information to individual quadrature rule
+    * @param x  quadrature rule type
+    * @return   answer
+    ************************************************/
     public static double compute(String x)
     {
         // Depending on the contents of the string,
