5acd20220151d980ab215679e4d12746e5cc766c
Dmitri Park <cellofortist@gmail.com>
2013-05-31 07:42:23 -0400

    hahdafh

--- quadrature/Display.java
+++ quadrature/Display.java
@@ -63,12 +63,12 @@ public class Display extends JPanel
     * Draws function on coordinate plane
     * @param g  Graphics object
     ***************************************************/
-    public static void drawPlot(Graphics g) throws IOException
+    public static void drawPlot(Graphics g)
     {
-       PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("data2.txt")));
-       try{ System.out.println("asdf");
-       } catch(IOException e)
-       { System.err.println("Caught IOException: " + e.getMessage());}
+       //PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("data2.txt")));
+       //try{ System.out.println("asdf");
+       //} catch(IOException e)
+       //{ System.err.println("Caught IOException: " + e.getMessage());}*/
        g.setColor(Color.BLACK); // Set color to black
 		 // Plot graph depending on graph type 
 		 // Set different color if needed
@@ -77,20 +77,21 @@ public class Display extends JPanel
        //if(a=3rl
        for(int i = 50; i<650; i++)
        {
-            double y = Math.pow((((double)i-360.0)/260.0), A);
+            double y = Math.pow((((double)i-360.0)/260.0), 0.5);
             int x = i; int y1 = (int)((280-(y*250)));
             if(bounded(x, y1))
                 {g.drawLine(x, y1, x, y1);
-                out.println(x+" "+y1);}
+                //out.println(x+" "+y1);
+                }
        }
     }        
     /***************************************************
     * Runs all other methods to draw function, axes, etc.
     * @param g  Graphics object
     ***************************************************/ 
-    public void paintComponent(Graphics g2) throws IOException 
+    public void paintComponent(Graphics g2)
     {
-	 	  // Set color to white
+	 	// Set color to white
         g2.setColor(Color.WHITE);
         // Draw filled rectangle at (50,20), width=600, height=530
         g2.fillRect(50, 20, 600, 530);
